666b3af692ee1ef8ea2e9fb4e3c6e9d753354866
Trollkopf <max.serratosa@gmail.com>
2023-03-24 14:44:04 +0100

    Add User Contact relation

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Contact extends Model
 {
@@ -13,6 +14,11 @@ class Contact extends Model
         "name",
         "phone_number",
         "age",
-        "email"
+        "email",
+        "user_id"
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -42,11 +42,14 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-// CONTACTS
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
-Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
-Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
+//CONTACTS CRUD
+Route::resource('contacts', ContactController::class);
+
+// CONTACTS CRUD MANUAL
+// Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+// Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+// Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+// Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+// Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+// Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+// Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
