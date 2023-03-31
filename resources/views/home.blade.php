4d8d440b6ae55d412ca60773f785f198abbb8980
Trollkopf <max.serratosa@gmail.com>
2023-03-31 08:59:38 +0200

    updated features (home/sass)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container pt-4 p-3">
-    <div class="row">
+    <div class="container pt-4 p-3">
+        <div class="row">
 
-      @forelse ($contacts as $contact)
-        <div class="col-md-4 mb-3">
-          <div class="card text-center">
-            <div class="card-body">
-              <div class="d-flex justify-content-center mb-2">
-                <a href="{{ route('contacts.show', $contact->id) }}">
-                  <img src="{{ Storage::url($contact->profile_picture) }}" class="profile-picture">
-                </a>
-              </div>
+            @forelse ($contacts as $contact)
+                <div class="col-md-4 mb-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-center mb-2">
+                                <a href="{{ route('contacts.show', $contact->id) }}">
+                                    <img src="{{ Storage::url($contact->profile_picture) }}" class="profile-picture">
+                                </a>
+                            </div>
 
-              <a href="{{ route('contacts.show', $contact->id) }}" class="text-decoration-none ">
-                <h3 class="text-warning text-capitalize">{{ $contact->name }}</h3>
-              </a>
-              <p class="m-2">{{ $contact->phone_number }}</p>
-              <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-secondary mb-2">Edit Contact</a>
-              <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger mb-2">Delete Contact</button>
-              </form>
+                            <a href="{{ route('contacts.show', $contact->id) }}" class="text-decoration-none ">
+                                <h3 class="text-warning text-capitalize">{{ $contact->name }}</h3>
+                            </a>
+                            <p class="m-2">{{ $contact->phone_number }}</p>
 
-            </div>
-          </div>
+                            <div class="d-flex justify-content-center">
+                                <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-secondary mb-2 me-2">Edit
+                                    Contact</a>
+                                <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger mb-2">Delete Contact</button>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <div class="col-md-4 mx-auto">
+                    <div class="card card-body text-center">
+                        <p>No contacts saved yet</p>
+                        <a href="{{ route('contacts.create') }}">Add One!</a>
+                    </div>
+                </div>
+            @endforelse
+
         </div>
-      @empty
-        <div class="col-md-4 mx-auto">
-          <div class="card card-body text-center">
-            <p>No contacts saved yet</p>
-            <a href="{{ route('contacts.create') }}">Add One!</a>
-          </div>
-        </div>
-      @endforelse
-
     </div>
-  </div>
 @endsection
